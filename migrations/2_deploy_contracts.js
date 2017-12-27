d1eae8e0fc99fe3e0a394c7adafc3e304fc741e3
Dmytro Saienko <dmsa@ciklum.com>
2017-12-27 12:05:14 +0200

    exchange_adding_tokens

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 var OrderBuyBook = artifacts.require("./OrderBuyBook.sol");
 var TokenABC = artifacts.require("./TokenABC.sol");
-var TokenXYZ = artifacts.require("./TokenABC.sol");
-var TokenCDF = artifacts.require("./TokenABC.sol");
+var TokenXYZ = artifacts.require("./TokenXYZ.sol");
+var TokenCDF = artifacts.require("./TokenCDF.sol");
 var Exchange = artifacts.require("./Exchange.sol");
 
 module.exports = function(deployer) {
--- test/exchange_adding_tokens.js
+++ test/exchange_adding_tokens.js
@@ -6,6 +6,7 @@ var Exchange = artifacts.require("./Exchange.sol");
 contract('exchange_adding_tokens', function(accounts) {
   it("should be possible to add tokens", function () {
       var tokenInstanceABC;
+      var tokenInstanceXYZ;
       var exchangeInstance;
 
       return TokenABC.deployed().then(function (instance) {
@@ -13,18 +14,24 @@ contract('exchange_adding_tokens', function(accounts) {
           return Exchange.deployed();
       }).then(function (instance) {
           exchangeInstance = instance;
-          return exchangeInstance.addToken(
-            tokenInstanceABC.symbol,
-            tokenInstanceABC.address
-          );
+          return exchangeInstance.addToken("ABC", tokenInstanceABC.address);
       }).then(function (txResult) {
           assert.equal(txResult.logs[0].event, "TokenAdded", "TokenAddedtoSystem Event should be emitted");
-          return exchangeInstance.hasToken.call(tokenInstanceABC.symbol);
+          return exchangeInstance.hasToken.call("ABC");
       }).then(function (booleanHasToken) {
-          assert.equal(booleanHasToken, true, "The Token was not added");
+          assert.equal(booleanHasToken, true, "The Token has been added");
           return exchangeInstance.hasToken.call("XYZ");
       }).then(function (booleanHasNotToken) {
-          assert.equal(booleanHasNotToken, false, "A Token that doesn't exist was found.");
+          assert.equal(booleanHasNotToken, false, "Token XYZ doesn't exist yet");
+          return TokenXYZ.deployed();
+      }).then(function (instance){
+          tokenInstanceXYZ = instance;
+          return exchangeInstance.addToken("XYZ", tokenInstanceXYZ.address);
+      }).then(function (txResult) {
+          assert.equal(txResult.logs[0].event, "TokenAdded", "TokenAddedtoSystem Event should be emitted");
+          return exchangeInstance.hasToken.call("XYZ");
+      }).then(function(booleanHasToken){
+          assert.equal(booleanHasToken, true, "Exchange has TokenXYZ");
       });
   });
 });
